dc03c36ffeeedf59ac442de0b448c52ce4899f66
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-07 00:27:30 +0330

    implement some common method of telegram bot and use middleware

--- Basic-Bot-Functions/bot.js
+++ Basic-Bot-Functions/bot.js
@@ -4,10 +4,6 @@ const bot = new Telegraf('1025160257:AAHMFYeiqHLJUmFGqSm60FbDlVnCl7hgiT8');
 
 //code 
 
-bot.start((ctx) => {
-    ctx.reply(ctx.from.first_name + " have entered start button and it is " + ctx.updateSubTypes[0])
-})
-
 bot.help((ctx) => {
     ctx.reply("you have entered help button")
 })
@@ -19,5 +15,35 @@ bot.settings((ctx) => {
 bot.command(["test" , "Test"] , (ctx) => {
     ctx.reply("Hello world !");
 })
+
+bot.hears("cow" , (ctx) => {
+    ctx.reply("MOOOOO ! :|");
+})
+
+bot.on("sticker" , (ctx) => {
+    ctx.reply("You sent sticker to " + ctx.botInfo.username);
+})
+
+bot.mention("basicFunctionBot" , (ctx) => {
+    ctx.reply("mention method called")
+})
+
+bot.phone("09198438113" , (ctx) => {
+    ctx.reply("phone method called")
+})
+
+bot.hashtag("AlirezaEbrahimkhani" , (ctx) => {
+    ctx.reply("hastag method called")
+})
+
+bot.use((ctx , next) => {
+    ctx.reply(ctx.from.first_name + " now use the bot :|");
+    next(ctx);
+})
+
+bot.start((ctx) => {
+    ctx.reply(ctx.from.first_name + " have entered start button and it is " + ctx.updateSubTypes[0])
+})
+
 
 bot.launch();
