aad78d91747e8a09990a34fe807e5bc648a43cc4
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-07 14:15:17 +0330

    implement the logger and log into a group

--- 1-echo-bot/bot.js
+++ 1-echo-bot/bot.js
@@ -9,17 +9,19 @@ Say somthing to me Please :
 `
 
 bot.use((ctx , next) => {
+    //log the sender in the group
+
     if(ctx.updateSubTypes[0] == "text"){
         if(Object.keys(ctx.from).includes("username")){
-            console.log(ctx.from.username + " said: " + ctx.message.text);
+            bot.telegram.sendMessage(-356034787 , ctx.from.username + " said: " + ctx.message.text + " to the bot")
         }else{
-            console.log(ctx.from.first_name + " said: " + ctx.message.text);
+            bot.telegram.sendMessage(-356034787 , ctx.from.first_name + " said: " + ctx.message.text + " to the bot")
         }
     }else{
         if(Object.keys(ctx.from).includes("username")){
-            console.log(ctx.from.username + " sent: " + ctx.updateSubTypes[0]);
+            bot.telegram.sendMessage(-356034787 , ctx.from.username + " sent: " + ctx.updateSubTypes[0] + " to the bot")
         }else{
-            console.log(ctx.from.first_name + " sent: " + ctx.updateSubTypes[0]);
+            bot.telegram.sendMessage(-356034787 , ctx.from.first_name + " sent: " + ctx.updateSubTypes[0] + " to the bot")
         }
     }
     next();
