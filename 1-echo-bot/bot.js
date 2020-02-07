120886379c1e4586ff00fba33e96f126d3584ed9
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-07 13:49:27 +0330

    implement logger for bot to know who sent message

--- 1-echo-bot/bot.js
+++ 1-echo-bot/bot.js
@@ -8,6 +8,23 @@ Say somthing to me Please :
 /help - command refrence
 `
 
+bot.use((ctx , next) => {
+    if(ctx.updateSubTypes[0] == "text"){
+        if(Object.keys(ctx.from).includes("username")){
+            console.log(ctx.from.username + " said: " + ctx.message.text);
+        }else{
+            console.log(ctx.from.first_name + " said: " + ctx.message.text);
+        }
+    }else{
+        if(Object.keys(ctx.from).includes("username")){
+            console.log(ctx.from.username + " sent: " + ctx.updateSubTypes[0]);
+        }else{
+            console.log(ctx.from.first_name + " sent: " + ctx.updateSubTypes[0]);
+        }
+    }
+    next();
+})
+
 bot.start((ctx) => {
     ctx.reply("Hello i am echo bot >_-");
     ctx.reply(helpMessage)
@@ -15,6 +32,23 @@ bot.start((ctx) => {
 
 bot.help((ctx) => {
     ctx.reply(helpMessage)
+})
+
+bot.command("echo" , (ctx) => {
+    let input  = ctx.message.text;
+
+    let inputArray = input.split(" ");
+
+    let message = "";
+
+    if(inputArray.length == 1){
+        message = "You entered echo command";
+    }else{
+        inputArray.shift();
+        message = inputArray.join(" ");
+    }
+
+    ctx.reply(message);
 })
 
 bot.launch();
