c1fcb071ab24035d11acae85eb226d0d2f637a0b
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-07 13:06:17 +0330

    start and help command implemented

--- 1-echo-bot/bot.js
+++ 1-echo-bot/bot.js
@@ -2,4 +2,19 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf('974246522:AAHCddzjyNfgLyhvLhp93bUa_-WZcDSW_7Q');
 
+const helpMessage = `
+Say somthing to me Please :
+/start - start the bot
+/help - command refrence
+`
+
+bot.start((ctx) => {
+    ctx.reply("Hello i am echo bot >_-");
+    ctx.reply(helpMessage)
+})
+
+bot.help((ctx) => {
+    ctx.reply(helpMessage)
+})
+
 bot.launch();
